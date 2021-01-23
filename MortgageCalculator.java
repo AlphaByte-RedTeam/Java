ee8a14957e285611a1d0b233521a9f471d3381c5
Andrew Virya Victorio <andrew.avv03@gmail.com>
2021-01-23 15:33:13 +0700

    Update MortgageCalculator.java

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -1,4 +1,4 @@
-package com.java.learn;
+package com.java.learn; // Just delete this guy if you want
 
 import java.text.NumberFormat;
 import java.util.Scanner;
@@ -19,7 +19,7 @@ public class MortgageCalculator {
 
         while( true ) {
             System.out.print( "Principal: Rp. " );   // Asking for the principal amount
-            principal = read.nextInt();             // Inputting principal amount
+            principal = read.nextInt();              // Inputting principal amount
             if( principal >= 1000 && principal <= 1_000_000 ) {
                 break;
             }
@@ -31,38 +31,44 @@ public class MortgageCalculator {
 
         while( true ) {
             System.out.print( "Annual Interest Rate: " );   // Asking for annual interest rate
-            annualInterest = read.nextFloat();             // Inputting the amount, Ex Input: 
-            if( annualInterest >= 1 && annualInterest <= 30) {
-                monthlyInterest = ( annualInterest / PERCENT ) / MONTHS_IN_YEAR;
+            annualInterest = read.nextFloat();              // Inputting the amount, Ex Input: 3.92 or 3,92
+            if( annualInterest >= 1 && annualInterest <= 30 ) {
+                monthlyInterest = ( annualInterest / PERCENT ) / MONTHS_IN_YEAR;    // Calculate the monthyl interest
                 break;
             }
 
             else {
-                System.out.println("Enter a value between 1 and 30");
+                System.out.println( "Enter a value between 1 and 30" );
             }
         }
 
-        while(true){
-            System.out.print("Period(Years): ");
+        while( true ){
+            System.out.print( "Period(Years): " );
             byte years = read.nextByte();
-            if(years >= 1 && years <= 30) {
-                numberOfPayments = years * MONTHS_IN_YEAR;
+            if( years >= 1 && years <= 30 ) {
+                numberOfPayments = years * MONTHS_IN_YEAR;  // Calculating the number of payments
                 break;
             }
 
             else {
-                System.out.println("Enter a value between 1 and 30");
+                System.out.println( "Enter a value between 1 and 30" );
             }
         }
 
-        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
-                          / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+        double mortgage = principal * ( monthlyInterest * Math.pow( 1 + monthlyInterest, numberOfPayments ) )
+                          / (Math.pow( 1 + monthlyInterest, numberOfPayments) - 1 );
+        
+        // The mortgage formula
 
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: " + mortgageFormatted);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format( mortgage ); // Get the currency
+        System.out.println( "Mortgage: " + mortgageFormatted );
     }
 }
 
 /*
+Input Example:
 
+Principal: Rp. 1000000
+Annual Interest Rate: 3.92 or 3,92
+Period(Years): 2
 */
